b6d55297f6bcaabb71fbf47958d1fbbcea3a7d84
Matteo Manzinello <matteo@manzinello.com>
2019-10-09 18:48:03 +0200

    go on with scss

--- src/github-fabulous.js
+++ src/github-fabulous.js
@@ -5,7 +5,7 @@ const gfCSS = document.createElement("style");
 gfCSS.id = "github-fabulous-style";
 gfCSS.type = "text/css";
 const gfCSSContent = document.createTextNode(`GITHUB_FABULOUS_STYLE`);
-maigfCSSlgoCSS.appendChild(gfCSSContent);
+gfCSS.appendChild(gfCSSContent);
 document.head.appendChild(gfCSS);
 
 // TODO
